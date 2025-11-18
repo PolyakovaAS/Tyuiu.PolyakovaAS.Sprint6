935f833493954e35c5f926187a5f8f83d16b89b5
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-18 14:21:31 +0300

    Task7

--- Tyuiu.PolyakovaAS.Sprint6.Task7.V25.Lib/DataService.cs
+++ Tyuiu.PolyakovaAS.Sprint6.Task7.V25.Lib/DataService.cs
@@ -8,22 +8,32 @@ namespace Tyuiu.PolyakovaAS.Sprint6.Task7.V25.Lib
     {
         public int[,] GetMatrix(string path)
         {
-            
+            // Читаем все строки из файла
             string[] lines = File.ReadAllLines(path);
 
-            
+            // Определяем размеры матрицы
             int rows = lines.Length;
             int cols = lines[0].Split(';').Length;
 
             int[,] matrix = new int[rows, cols];
 
-            
+            // Заполняем матрицу данными из файла
             for (int i = 0; i < rows; i++)
             {
                 string[] values = lines[i].Split(';');
                 for (int j = 0; j < cols; j++)
                 {
                     matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            // Преобразуем матрицу: заменяем отрицательные элементы в 7-м столбце на 2
+            for (int i = 0; i < rows; i++)
+            {
+                // 7-й столбец имеет индекс 6 (нумерация с 0)
+                if (matrix[i, 6] < 0)
+                {
+                    matrix[i, 6] = 2;
                 }
             }
 
