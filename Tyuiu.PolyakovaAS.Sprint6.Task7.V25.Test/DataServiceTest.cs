3476fcbcd474239abb268bbef193872a5f0c69fc
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-18 14:18:44 +0300

    Task7

---
+++ Tyuiu.PolyakovaAS.Sprint6.Task7.V25.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.PolyakovaAS.Sprint6.Task7.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
