2177b65c8c87b0f2066fe30bb0c510714e21950c
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-18 13:13:52 +0300

    Add project files.

---
+++ Tyuiu.PolyakovaAS.Sprint6.Task0.V28.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.PolyakovaAS.Sprint6.Task0.V28.Lib
+{
+    public class DataService : ISprint6Task0V28
+    {
+        public double Calculate(int x)
+        {
+            return Math.Round(6.1 * Math.Pow(x, 3) + 0.23 * Math.Pow(x, 2) + 1.04 * x, 3);
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAS.Sprint6.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PolyakovaAS.Sprint6.Task0.V28.Lib;
+
+namespace Tyuiu.PolyakovaAS.Sprint6.Task0.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 169.89;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
